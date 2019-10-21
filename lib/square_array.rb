ce56b9981a5ada3ca97e5fdb665febc292fd4d59
Christian Jimenez <christianjimenez0123@gmail.com>
2019-10-21 22:02:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,4 @@
 def square_array(numbers)
-  counter = 0
-  square_array = []
-  numbers = [1,2,3]
   while counter <= numbers.length do
     square_array.insert(numbers[counter] ** 2)
     counter += 1
